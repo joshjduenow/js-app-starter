80c78eef60eb8d4549f7eab44dfa286f45c5881b
Joshua Duenow <joshjduenow@gmail.com>
2023-10-27 10:50:50 -0500

    through step one and step two

--- script.js
+++ script.js
@@ -2,4 +2,49 @@ function onReady() {
   console.log('Javascript still works! What a gosh darn relief.');
 }
 
+let todoCounter = 0;
+
+function handleSubmit(event) {
+  //stop the form from taking over refreshing
+  event.preventDefault();
+
+  console.log('did this work?');
+  
+  //go get the input
+  let todoText = document.getElementById('todo-text').value;
+  let todoAuthor = document.getElementById('todo-author').value;
+  
+  //set the value to be an empty string
+  document.getElementById('todo-text').value = '';
+  document.getElementById('todo-author').value = '';
+  
+  
+  console.log(todoText, todoAuthor);
+  
+  let todoList = document.getElementById('todoList');
+
+  todoList.innerHTML += `
+  <li>
+      <button onclick="deleteTodo(event)">❌</button>
+      ${todoText} (${todoAuthor})
+  </li>
+  `
+
+  todoCounter++;
+  console.log(todoCounter);
+
+  document.getElementById('todoCounter').textContent = todoCounter
+
+}
+function deleteTodo(event) {
+  
+  console.log('trying to delete');
+  
+  event.target.parentElement.remove();
+  
+  todoCounter--;
+  console.log(todoCounter);
+  
+  document.getElementById('todoCounter').textContent = todoCounter
+}
 onReady();
